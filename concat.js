2a688a66e688fdb18bb052bdc2cd79f80e6df7a8
plabondas001 <plabondas001@gmail.com>
2025-10-15 18:52:43 +0600

    concat

---
+++ concat.js
@@ -0,0 +1,8 @@
+// const fir = 'plabon';
+// const last = 'das';
+// const name = fir + last;
+// console.log(name);
+const first = 'plabon';
+const last = 'das';
+const name = first + ' ' + last;
+console.log(name);
