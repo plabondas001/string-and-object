9d4148da1c737bab4ffcf71de154cbe782517a4e
plabondas001 <plabondas001@gmail.com>
2025-10-15 18:53:31 +0600

    slice

---
+++ slice.js
@@ -0,0 +1,10 @@
+const name = 'plabon';
+const nam = name.slice(1, 5);
+// console.log(nam);
+
+
+// split
+
+const sen = 'I am a good wev dev';
+// console.log(sen.split(' '));
+console.log(sen.includes('u'));
