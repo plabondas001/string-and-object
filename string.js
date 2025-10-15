2aca4c162b3b517eea49b192305c5d196160fb17
plabondas001 <plabondas001@gmail.com>
2025-10-15 18:54:17 +0600

    string

---
+++ string.js
@@ -0,0 +1,15 @@
+const country = 'Bangaldesh';
+const division = 'Dhaka';
+const district = `Kishoreganj`;
+// const thana = new string('Bajitpur');
+// console.log(typeof district);
+
+
+// const numbers = ['apple', 'lico', 'kola', 'jam'];
+// numbers[1]= 'jambora';
+// console.log(numbers[1]);
+
+
+const place = 'Dhaka';
+place[1] = 'F'
+console.log(place);
